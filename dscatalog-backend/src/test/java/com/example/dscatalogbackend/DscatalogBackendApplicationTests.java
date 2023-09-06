75ffeba951db5398efaf264777ce0df74218ba55
Yago Garcez <yago.garcez@outlook.com>
2023-09-05 21:49:51 -0300

    Backend project created.

---
+++ dscatalog-backend/src/test/java/com/example/dscatalogbackend/DscatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dscatalogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
